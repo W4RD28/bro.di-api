28f4253f1aa704a01680d02867aae1a0111dc9bf
Windu Nursetyadi <windudr@gmail.com>
2022-05-11 15:07:03 +0700

    added cors functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('@prisma/client');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -9,6 +10,13 @@ app.use(bodyParser.json())
 
 // redirect to routes/index.js
 const route = require('./routes');
+app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use('/api/v1/', route);
 
 const port = process.env.PORT || 5000;
